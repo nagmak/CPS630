e8129cd369cf3291fb31375b6182c1b0859b69d6
dylan1313 <ddonsky@hotmail.com>
2020-02-05 16:29:31 -0500

    added php useability and prints

---
+++ lab2/lab2.php
@@ -0,0 +1,12 @@
+
+<?php
+    echo $_REQUEST["creativecommons"];
+    echo $_REQUEST["type"];
+    echo $_REQUEST["museum"];
+    echo $_REQUEST["year"];
+    echo $_REQUEST["subject"];
+    echo $_REQUEST["genre"];
+    echo $_REQUEST["description"];
+    echo $_REQUEST["title"];
+?>
+
