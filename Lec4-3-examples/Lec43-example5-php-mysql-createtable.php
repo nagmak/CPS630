0e248131229473805fc6af7a293c5270ce96ba66
Nagma Kapoor <nagmakapoor@gmail.com>
2020-03-19 20:23:44 -0400

    adding more labs

---
+++ Lec4-3-examples/Lec43-example5-php-mysql-createtable.php
@@ -0,0 +1,34 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "testnew";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE StRec2 (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Student Records created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?> 
+
+
+
+ 
